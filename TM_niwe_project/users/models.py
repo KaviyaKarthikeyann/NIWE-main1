09501892d0bf6a96fe09d5b44fbe0de7588f9a16
KaviyaKarthikeyann <kaviya.e0219062@sret.edu.in>
2021-11-16 19:13:52 +0530

    first commit

---
+++ TM_niwe_project/users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class NIWETable(models.Model):
+    id = models.IntegerField(primary_key=True)
+    sendermail = models.CharField(max_length=50)
+    subject = models.CharField(max_length=100)
+    receievermail = models.CharField(max_length=50)
+    ccmail = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    remarks = models.CharField(max_length=200)
+    duedate = models.DateField()
+
+    class Meta:
+        db_table = "niwe"
+# Create your models here.
